b6653ab76b1377749bb78e563958b26c790a5077
KNstntin <41593496+KNstntin@users.noreply.github.com>
2021-01-29 02:13:13 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='cycle_gan_tools',
+    url='https://github.com/KNstn/cycle_gan_jojo',
+    author='NikolaevKA',
+    author_email='kanikolaev_1@edu.hse.ru',
+    packages=['packages'],
+    install_requires=['numpy', 'torch', 'torchvision', 'PIL'],
+    version='0.1',
+    license='MIT',
+    description='Some tools for work with CycleGAN'
+)
