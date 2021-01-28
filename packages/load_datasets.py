bcb7cdf67314c46886794d5fa292329d350cda03
KNstntin <41593496+KNstntin@users.noreply.github.com>
2021-01-29 02:34:54 +0300

    Update load_datasets.py

--- packages/load_datasets.py
+++ packages/load_datasets.py
@@ -1,7 +1,7 @@
 import urllib.request
 
 def real2jojo():
-    url = "https://www.dropbox.com/s/ukeygzg9ywnnapg/real2jojo.zip?dl=1"  # dl=1 is important
+    url = "https://www.dropbox.com/s/ukeygzg9ywnnapg/real2jojo.zip?dl=1"
     zip_file = urllib.request.urlopen(url)
     data = zip_file.read()
     zip_file.close()
@@ -9,7 +9,7 @@ def real2jojo():
         f.write(data)
 
 def horse2zebra():
-    url = "https://www.dropbox.com/s/xm7c5jw2ut389o6/horse2zebra.zip?dl=1"  # dl=1 is important
+    url = "https://www.dropbox.com/s/xm7c5jw2ut389o6/horse2zebra.zip?dl=1"
     zip_file = urllib.request.urlopen(url)
     data = zip_file.read()
     zip_file.close()
